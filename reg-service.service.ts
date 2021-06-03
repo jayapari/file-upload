7863932da48908b9fb3a9c29d0fdfb5b5c515f1e
jayapari <78636954+jayapari@users.noreply.github.com>
2021-06-03 19:09:51 +0530

    Create reg-service.service.ts

---
+++ reg-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegServiceService {
+   _url = 'http://localhost:3000/enroll';
+
+  constructor(private _http: HttpClient) { }
+
+  register(userData: any)
+  {
+    return this._http.post<any>(this._url,userData);
+  }
+}
